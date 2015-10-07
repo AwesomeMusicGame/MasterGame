6ab9bb6215b604337fe6586f4670191e3d2b970f
AwesomeMusicGame <1x16x9+76e29nzxcczp0@sharklasers.com>
2015-10-07 20:46:04 +0100

    spraveny Import
    bude treba troksu upravit spawn kociek, a aj cestu importovania, ale ako
    naimportovat naimportje

---
+++ Assets/ImportFromTxt.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class ImportFromTxt : MonoBehaviour
+{
+
+    private Kolajnice kolajniceScript;
+
+	// Use this for initialization
+	void Start () {
+        kolajniceScript = GameObject.FindGameObjectWithTag("KolajniceTag").GetComponent<Kolajnice>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.L))
+        {
+            ImportFromFile();
+            kolajniceScript.spawnMapTemp = true;
+        }
+	}
+
+    public void ImportFromFile()
+    {
+        List<float> tempList = new List<float>();
+        try
+        {
+            //need to be fixed 
+            StreamReader reader = new StreamReader(@"D:\Hearthstone\MasterGame\Assets\Stone Magnet.txt");
+            string input = reader.ReadToEnd();
+            if (input != string.Empty)
+            {
+                string[] parts = input.Split('\n');
+                foreach (string part in parts)
+                {
+                    float tempFloat;
+                    float.TryParse(part, out tempFloat);
+                    tempList.Add(tempFloat);
+                    Debug.Log(tempFloat + " parsed from: " + part);
+                }
+                kolajniceScript.WriteAllBeats();
+                kolajniceScript.SetBeats(tempList);
+                reader.Close();
+            }
+            else
+            {
+                Debug.Log("No lvl data loaded from the file...string is empty");
+            }
+        }
+        catch (NullReferenceException e)
+        {
+            Debug.Log("Null Reference Exception.\n" + e.ToString());
+        }
+        catch (FileNotFoundException e)
+        {
+            Debug.Log("No lvl data loaded from the file...file is missing\n" + e.ToString());
+        }
+
+
+    }
+}
--- Assets/Kolajnice.cs
+++ Assets/Kolajnice.cs
@@ -1,40 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Kolajnice : MonoBehaviour {
 
     public GameObject kockaPrefab;
     public GameObject prekazkaPrefab;
     public float sideDistance = 5;
+    public bool spawnMapTemp = false;
 
-    private ArrayList beats;
+    private List<float> beats = new List<float>();
 
 	// Use this for initialization
 	void Start () {
+               
+	}
 
+    public List<float> GetBeats()
+    {
+        return beats;
+    }
+
+    public void SetBeats(List<float> tempList)
+    {
+        beats = tempList;
+    }
+
+    public void WriteAllBeats()
+    {
+        foreach (float Beat in beats)
+        {
+            Debug.Log(Beat + "\n");
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (spawnMapTemp == true)
+        {
+            SpawnMap();
+            spawnMapTemp = false;
+        }
+        this.transform.position = new Vector3(0, 0, -sideDistance);
+	}
+
+    void SpawnMap()
+    {
         float beatlenght = 0.5f;
         int lastrow = -1;
-        for (float i = 0; i < 300; i += beatlenght)
+        // for (float i = 0; i < 300; i += beatlenght)
+        for (int i = 0; i < beats.Count; i++)
         {
-            float positionOffset = (kockaPrefab.transform.localScale.z / 2);
+            float positionOffset = (kockaPrefab.transform.localScale.z/2);
             int row = Random.Range(0, 3);
             if (lastrow == row)
             {
-                GameObject tempPrekazka = (GameObject)Instantiate(prekazkaPrefab, new Vector3(i, 0, sideDistance * row + positionOffset), Quaternion.identity);
-                tempPrekazka.transform.parent = this.transform;
+                 GameObject tempPrekazka = (GameObject)Instantiate(prekazkaPrefab, new Vector3(i, 0, sideDistance * row + positionOffset), Quaternion.identity);
+                 tempPrekazka.transform.parent = this.transform;
             }
             lastrow = row;
             //beats.Add(i);
-            GameObject temp = (GameObject) Instantiate(kockaPrefab, new Vector3(i, 0, sideDistance * row + positionOffset), Quaternion.identity);
+            GameObject temp = (GameObject)Instantiate(kockaPrefab, new Vector3(i, 0, sideDistance * row + positionOffset), Quaternion.identity);
             temp.GetComponent<Stretching>().lenght = beatlenght;
             temp.transform.parent = this.transform;
         }
-
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        this.transform.position = new Vector3(0, 0, -sideDistance);
-	}
+    }
 }
