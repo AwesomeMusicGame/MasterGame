94c9dfc19871bc0aab391f5914af6475711bef23
AwesomeMusicGame <1x16x9+76e29nzxcczp0@sharklasers.com>
2015-10-14 22:09:17 +0100

    Menu added with functionality

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+    public Menu currentMenu;
+
+    public void Start()
+    {
+        ShowMenu(currentMenu);
+    }
+
+    public void ShowMenu(Menu menu)
+    {
+        if(currentMenu != null)
+        {
+            currentMenu.IsOpen = false;
+        }
+        
+        currentMenu = menu;
+        currentMenu.IsOpen = true;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadLevel(int level)
+    {
+        Application.LoadLevel(level);
+    }
+}
