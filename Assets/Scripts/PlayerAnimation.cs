22d5c660c1f78120fbee8ace2d4a9c8584e5559e
Jana Němcová <nemcova@ics.muni.cz>
2015-10-31 00:23:35 +0100

    punch animation added

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -16,20 +16,31 @@ public class PlayerAnimation : MonoBehaviour {
             GetComponent<Animator>().SetBool("jump", true);
         }
 
-        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
         {
             GetComponent<Animator>().SetBool("jump", false);
         }
 
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            GetComponent<Animator>().SetBool("bend", true);
+            GetComponent<Animator>().SetBool("punch", true);
+        }     
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            GetComponent<Animator>().SetBool("punch", false);
         }
 
         if (Input.GetKeyUp(KeyCode.DownArrow))
         {
             GetComponent<Animator>().SetBool("bend", false);
         }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            GetComponent<Animator>().SetBool("bend", true);
+        }
+
     }
 }
